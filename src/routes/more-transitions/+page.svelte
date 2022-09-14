6eb6df3db0cb465e87c8aae6492946cdea56e659
mattcroat <mattcroat.dev@gmail.com>
2022-09-14 21:17:07 +0200

    feat: Add draw example

--- src/routes/more-transitions/+page.svelte
+++ src/routes/more-transitions/+page.svelte
@@ -32,6 +32,29 @@
 		<div class="transition">
 			<span in:scale={{ duration: 1000 }}>Scale</span>
 		</div>
+
+		<div class="transition">
+			<svg
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				width="48px"
+				height="48px"
+				stroke="currentColor"
+				stroke-width="1.4"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				fill="none"
+			>
+				<path
+					in:draw={{ duration: 1500 }}
+					d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+				/>
+				<polyline
+					in:draw={{ duration: 2000 }}
+					points="22 4 12 14.01 9 11.01"
+				/>
+			</svg>
+		</div>
 	</div>
 </Animate>
 
@@ -55,5 +78,6 @@
 	span {
 		font-size: 2rem;
 		font-weight: 700;
+		text-shadow: 2px 0 10px hsl(0 0% 0% / 20%);
 	}
 </style>
