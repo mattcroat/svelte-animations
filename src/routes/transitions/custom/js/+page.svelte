56b2ca2980dbe6317932acfb85946744b4ade8e7
mattcroat <mattcroat.dev@gmail.com>
2022-09-19 12:07:14 +0200

    feat: Add custom JavaScript transition example

---
+++ src/routes/transitions/custom/js/+page.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import Animate from '$lib/animate.svelte'
+
+	let audio: HTMLAudioElement
+
+	onMount(() => {
+		audio = new Audio('/audio/typewriter.mp3')
+		audio.volume = 0.4
+		audio.loop = true
+	})
+
+	function typewriter(
+		node: HTMLElement,
+		{ speed = 1 } = {}
+	) {
+		const text = node.textContent!
+		const duration = text.length / (speed * 0.01)
+
+		return {
+			duration,
+			tick: (t: number) => {
+				const i = Math.trunc(text.length * t)
+				node.textContent = text.slice(0, i)
+			}
+		}
+	}
+
+	const play = () => audio.play()
+	const pause = () => audio.pause()
+</script>
+
+<Animate>
+	<p
+		in:typewriter
+		on:introstart={play}
+		on:introend={pause}
+		class="text"
+	>
+		“There are no mistakes, only happy accidents.”
+	</p>
+</Animate>
+
+<div class="typewriter" />
+
+<style lang="scss">
+	.typewriter {
+		width: 100vw;
+		height: 100vh;
+		background-image: url('/images/typewriter.webp');
+		background-size: cover;
+		background-position: center;
+	}
+
+	.text {
+		width: 100%;
+		position: absolute;
+		top: 20%;
+		padding: 0 3rem;
+		font-family: 'Special Elite', cursive;
+		font-size: 3rem;
+		color: hsl(220 20% 20% / 80%);
+	}
+</style>
