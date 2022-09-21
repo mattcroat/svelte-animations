b357daec77538f5c354322bf5dd8169485bba3a9
mattcroat <mattcroat.dev@gmail.com>
2022-09-21 16:29:04 +0200

    feat: Add prefers reduced motion example

--- src/routes/accessibility/+page.svelte
+++ src/routes/accessibility/+page.svelte
@@ -1,1 +1,10 @@
-<h1>Prefers Reduced Motion</h1>
+<script lang="ts">
+	import { getPrefersReducedMotion } from './media'
+
+	const prefersReducedMotion = getPrefersReducedMotion()
+
+	// change animation value based on user preference
+	const value = prefersReducedMotion ? 'Yes' : 'No'
+</script>
+
+<h1>Prefers Reduced Motion: {value}</h1>
